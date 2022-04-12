887cc4d7134195cf7da1b8cfc24a53757bd7fc03
yiinuma <y.iinuma211@gmail.com>
2022-04-12 21:23:59 +0900

    lesson4

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -33,3 +33,42 @@ export function getSortedPostsData() {
     }
   });
 }
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // 以下のような配列を返します:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    };
+  });
+}
+
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // 投稿のメタデータ部分を解析するために gray-matter を使う
+  const matterResult = matter(fileContents);
+
+  // データを id と組み合わせる
+  return {
+    id,
+    ...matterResult.data,
+  };
+}
